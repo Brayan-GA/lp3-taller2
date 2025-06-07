a15067f44576d80159626d5bde41802cea808e3c
Brayan Smith <brayan@B.localdomain>
2025-06-07 14:16:55 -0500

    Se importa CORS

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ Script principal para ejecutar la aplicación Flask.
 import os
 from musica_api import create_app
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 # TODO: Cargar variables de entorno desde archivo .env si existe
 load_dotenv()
 
 # TODO: crear la aplicación
 app = create_app()
+CORS(app)
 
 if __name__ == "__main__":
     # TODO: Obtener puerto del ambiente o usar 5000 por defecto
